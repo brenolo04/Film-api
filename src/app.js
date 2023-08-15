336044d512f3a02def54b801d21acda194d847a4
Breno Fonseca <brenocaires04@gmail.com>
2023-08-15 17:13:43 -0300

    start app and connecting with routes

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = require('./routes/index.routes')
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+const PORT = 8081;
+app.listen(PORT, () => console.log(`Listening at ${PORT}`));
